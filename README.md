cc53961d61c92565c29107220fd24c2571cbbd31
jose castellanos <j.castellanos.molina@accenture.com>
2022-02-22 09:30:52 -0600

    Added WebServices to Intercat with Resources

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # KubeEdge sample rpirelay
 
 
+
+kubectl create clusterrolebinding service-reader-pod --clusterrole=service-reader  --serviceaccount=default:default
+
--- edgecore/webclient/server.go
+++ edgecore/webclient/server.go
@@ -1,88 +1,86 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"flag"
-	"path/filepath"
-	"github.com/labstack/echo/v4"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-	"k8s.io/client-go/dynamic"
 	"encoding/json"
-	)
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+)
 
 var dynamicClient dynamic.Interface
 
 func GetRelays(c echo.Context) error {
-	
-	
-	result := getRelays(dynamicClient )
 
-	return c.JSON(http.StatusOK, result )
+	result := getRelays(dynamicClient)
+
+	return c.JSON(http.StatusOK, result)
 }
 
 // http://localhost:1323/device/relay-instance-01
 func GetRelayState(c echo.Context) error {
 	deviceName := c.Param("data")
-	
-	_, ch1Value, ch2Value, ch3Value := getRelayState(dynamicClient, deviceName )
+
+	_, ch1Value, ch2Value, ch3Value := getRelayState(dynamicClient, deviceName)
 
 	return c.JSON(http.StatusOK, map[string]string{
-        "device": deviceName,
-        "ch1": ch1Value,
-		"ch2": ch2Value,
-		"ch3": ch3Value,})
+		"device": deviceName,
+		"ch1":    ch1Value,
+		"ch2":    ch2Value,
+		"ch3":    ch3Value})
 }
 
 func PathRelayState(c echo.Context) error {
 	type DeviceRelay struct {
-		Device string `json:"device"`
+		Device   string `json:"device"`
 		CH1Value string `json:"ch1"`
 		CH2Value string `json:"ch2"`
 		CH3Value string `json:"ch3"`
-	 }
-	 deviceRelay := DeviceRelay{}
-	 defer c.Request().Body.Close()
-	 err := json.NewDecoder(c.Request().Body).Decode(&deviceRelay)
-	 
-	 if err != nil {
-		fmt.Printf("Failed reading the request body %s\n", err )
+	}
+	deviceRelay := DeviceRelay{}
+	defer c.Request().Body.Close()
+	err := json.NewDecoder(c.Request().Body).Decode(&deviceRelay)
+
+	if err != nil {
+		fmt.Printf("Failed reading the request body %s\n", err)
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error)
-	 }
+	}
 
-	 fmt.Printf("\nDevice: %s\n[CH1]: %s\n[CH2]: %s\n[CH3]: %s\n", deviceRelay.Device , deviceRelay.CH1Value, deviceRelay.CH2Value, deviceRelay.CH3Value )
+	fmt.Printf("\nDevice: %s\n[CH1]: %s\n[CH2]: %s\n[CH3]: %s\n", deviceRelay.Device, deviceRelay.CH1Value, deviceRelay.CH2Value, deviceRelay.CH3Value)
 
-	 err = setRelayState(dynamicClient, deviceRelay.Device , deviceRelay.CH1Value, deviceRelay.CH2Value, deviceRelay.CH3Value) 
+	err = setRelayState(dynamicClient, deviceRelay.Device, deviceRelay.CH1Value, deviceRelay.CH2Value, deviceRelay.CH3Value)
 
-	 if err != nil {
-		fmt.Printf("Failed executing set relay state %s\n", err )
+	if err != nil {
+		fmt.Printf("Failed executing set relay state %s\n", err)
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error)
-	 }
+	}
 
-	 return c.JSON(http.StatusOK, map[string]string{
-        "device": deviceRelay.Device,
-        "ch1": deviceRelay.CH1Value,
-		"ch2": deviceRelay.CH2Value,
-		"ch3": deviceRelay.CH3Value,})
+	return c.JSON(http.StatusOK, map[string]string{
+		"device": deviceRelay.Device,
+		"ch1":    deviceRelay.CH1Value,
+		"ch2":    deviceRelay.CH2Value,
+		"ch3":    deviceRelay.CH3Value})
 }
 
 func main() {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+	// var kubeconfig *string
+	// if home := homedir.HomeDir(); home != "" {
+	// 	kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	// } else {
+	// 	kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	// }
+	// flag.Parse()
 
 	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	// config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
 
-    dynamicClient, _ = dynamic.NewForConfig(config)
+	dynamicClient, _ = dynamic.NewForConfig(config)
 
 	e := echo.New()
 
@@ -90,11 +88,11 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	e.GET("/device",GetRelays)
+	e.GET("/device", GetRelays)
 
-	e.GET("/device/:data",GetRelayState)
+	e.GET("/device/:data", GetRelayState)
 
 	e.POST("/device", PathRelayState)
-	
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
